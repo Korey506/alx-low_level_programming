9be455befb1f3f6c69ba133e445a0e3d9e1309d6
Korey506 <ugbanaadebisi@yahoo.com>
2023-02-17 15:28:30 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char z = 'a';
+		while (z <= 'Z')
+		{
+			putchar(z);
+			z++;
+		}
+		z = 'A'
+		while (z <= 'Z')
+		{
+			putchar(z);
+			z++;
+		}
+		putchar('\n');
+		return (0);
+}
