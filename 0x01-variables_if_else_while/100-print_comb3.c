cfc0182ddbcb83b6fccdd981c1cd6335cf244109
Korey506 <ugbanaadebisi@yahoo.com>
2023-02-17 18:09:58 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,33 +3,30 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all possible different combinations of three digits,
- * separated by ", " and printed in ascending order, with three digits
+ * Description: Prints all possible different combinations of two digits,
+ * separated by ", " and printed in ascending order, with two digits
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j < 9; j++)
-		{
-			for (k = j + 1; k <= 9; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
+	for (j = i + 1; j <= 9; j++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
 
-	if (i != 7 || j != 8 || k != 9)
+	if (i != 8 || j != 9)
 	{
 		putchar(',');
+		putchar(' ');
 	}
-			}
-		}
 	}
+}
 
 	putchar('\n');
 
